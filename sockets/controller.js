cc0010dff03ee977313fc1e27f4b867030508850
Alejandro2097 <alejo12381@hotmail.es>
2021-12-26 19:34:31 -0500

    se agrega la clase y funcionalidad de crear tickets

--- sockets/controller.js
+++ sockets/controller.js
@@ -4,15 +4,14 @@ const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
 
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
     socket.on('disconnect', () => { });
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    socket.on('siguiente-ticket', ( payload, callback ) => {
         
-        const id = 123456789;
-        callback( id );
-
-        socket.broadcast.emit('enviar-mensaje', payload );
-
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+        //TODO: Notificar que hay un nuevo ticket pendiente de asignar
     });
 
 }
